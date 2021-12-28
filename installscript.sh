1e959e4a2ef102781de2d2bf8f4fa5d919de9e9a
nalinwadhwa02 <nalin.wadhwa02@gmail.com>
2021-12-28 19:23:49 +0530

    init

---
+++ installscript.sh
@@ -0,0 +1,12 @@
+# change repo to unstable
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
+cp sources.list /etc/apt/sources.list 
+# update
+apt update
+# add essentials
+# add xorg/utilities
+# add window manager/utlities
+apt install neofetch git vim htop unzip xorg i3-gapps i3blocks i3status suckless-tools i3lock nnn feh mpv
+# config window manager/utilities
+# add extra
+# make configs
